18afc74e5a39d060dce21e89a4f37430531fc8dc
Ruslex1234 <thero1234@hotmail.com>
2022-10-07 13:55:09 -0700

    Update beautiful.py

--- beautiful.py
+++ beautiful.py
@@ -11,118 +11,117 @@ import os
 import time
 import boto3
 # To work with the .env file
-from dotenv import load_dotenv
-load_dotenv()
-html = ""
-commanders=[
-    "Mudin Hazered",
-    "Frozen Thug",
-    "Emperor Hazered",
-    "Pess Joeru",
-    "Lady Hermionee",
-    "Deniel Villa",
-    "Frozen Camisadez",
-    "Chicken Bacon Onion",
-    "Tona Mentado",
-    "Nina Goodenough",
-    "Anferz villa",
-    "Dereck Goodenouugh",
-    "Manferz",
-    "Mikeilita",
-    "Demonique",
-    "Mirricuh",
-    "Beadu",
-    "Masonw",
-    "Mason Modo Zen",
-    "Masxn",
-    "Sutaw",
-    "Suta",
-    "Suta Belico",
-    "Suta Chiflado",
-    "Suta Mal",
-    "Invictus Frozen",
-    "Mudin Darksoul",
-    "Kuba Powtorka Zrozrywki",
-    "Bossy Blonde",
-    "Taina Rush",
-    "Nimbuz Prime",
-    "Nimbuz Powtorka Zrozrywki",
-    "Omar Infalible",
-    "Omar Vlaze",
-    "Ryno Infalible",
-    "Rynoz Infalible",
-    "Diex Gera",
-    "Gera",
-    "Gera Belico",
-    "Gera Pro Sio",
-    "Tuf Before",
-    "Anferz Insane",
-    "Manferz",
-    "Anferz Insane",
-    "Diex Torridzin",
-    "Ancorsz",
-    "Hippopotabner",
-    "Ancor",
-    "Tank Ferz"
-    ]
 
+html = ""
+#commanders=[
+#    "Mudin Hazered",
+#    "Frozen Thug",
+#    "Emperor Hazered",
+#    "Pess Joeru",
+#    "Lady Hermionee",
+#    "Deniel Villa",
+#    "Frozen Camisadez",
+#    "Chicken Bacon Onion",
+#    "Tona Mentado",
+#    "Nina Goodenough",
+#    "Anferz villa",
+#    "Dereck Goodenouugh",
+#    "Manferz",
+#    "Mikeilita",
+#    "Demonique",
+#    "Mirricuh",
+#    "Beadu",
+#    "Masonw",
+#    "Mason Modo Zen",
+#    "Masxn",
+#    "Sutaw",
+#    "Suta",
+#    "Suta Belico",
+#    "Suta Chiflado",
+#    "Suta Mal",
+#    "Invictus Frozen",
+#    "Mudin Darksoul",
+#    "Kuba Powtorka Zrozrywki",
+#    "Bossy Blonde",
+#    "Taina Rush",
+#    "Nimbuz Prime",
+#    "Nimbuz Powtorka Zrozrywki",
+#    "Omar Infalible",
+#    "Omar Vlaze",
+#    "Ryno Infalible",
+#    "Rynoz Infalible",
+#    "Diex Gera",
+#    "Gera",
+#    "Gera Belico",
+#    "Gera Pro Sio",
+#    "Tuf Before",
+#    "Anferz Insane",
+#    "Manferz",
+#    "Anferz Insane",
+#    "Diex Torridzin",
+#    "Ancorsz",
+#    "Hippopotabner",
+#    "Ancor",
+#    "Tank Ferz"
+#    ]
+#
 
 whole_list = {}
 total = {}
 main = {}
 maker = {}
 
+#def lambda_handler(event, context):
 #api_key = os.getenv('ALPHAVANTAGE_API_KEY')
 url ="https://www.tibia.com/community/?subtopic=guilds&page=view&GuildName={}"
-guilds = ['Gold Reappers','Alzados','Diamond Reappers','Godslayers','Reappers','Watch Os','Unstopables','savage order','dinastia belica']
+guilds = ['godslayers','reappers','diamond reappers', 'Acord Os']
+#guilds = ['Gold Reappers','Diamond Reappers','Godslayers','Reappers','Watch Os','Unstopables','Dinastia Camba','dinastia belica']
 #friend_guilds = ['Dinastia Alliance','Dinastia de Perrones','Flameborn','Army Airdrop','Fallens', 'Eternity','True Hope Emera', 'Push Back']
 guilds_web = []
 for each in guilds:
     guilds_web.append(each.replace(" ","+"))
-results = []
+results = []    
 
 start = time.time()
 
-def get_tasks(session):
-    tasks = []
-    for guild in guilds_web:
-        tasks.append(asyncio.create_task(session.get(url.format(guild), ssl=False)))
-    return tasks
-
-async def get_symbols():
-    async with aiohttp.ClientSession() as session:
-        tasks = get_tasks(session)
-        responses = await asyncio.gather(*tasks)
-        for response in responses:
-            global total
-            html_content = await response.text()
-            soup = BeautifulSoup(html_content, "lxml")
-            soup = unicodedata.normalize("NFKD",soup.text)
-            guild_name = re.search("Players Online(.|\n)*Guild Information",soup).group(0).splitlines()[12]
-            server = re.search("The guild was founded on (.*) on",soup).group(1)
-            text =  re.search(".*Joining DateStatus(.|\n)*Invited Characters",soup).group(0).splitlines()
-            if 'leadership of this guild' in text[-3]:
-                if 'offline' in text[-3]:
-                    text[-3]='offline'
-                else:
-                    text[-3]='online'
-                text.pop()
-            text = text[1:-1]
-            def divide_chunks(l, n):
-                for i in range(0, len(l), n): 
-                    yield l[i:i + n]
-            n = 5
-            x = list(divide_chunks(text, n))
-            if server not in whole_list:
-                whole_list[server] = []
-            if server in total:
-                total[server]= total[server] + int(len(x))
-            else:
-                total[server]= int(len(x))
-            for each in x:
-                if "online" in each[4]:
-                    whole_list[server].append([each[0],each[1],each[2],guild_name])
-
+for each in guilds_web:
+    response = requests.get(url.format(each))
+    html_content = response.text
+    soup = BeautifulSoup(html_content, "lxml")
+    soup = unicodedata.normalize("NFKD",soup.text)
+    #print(html_content)
+    guild_name = re.search("Players Online(.|\n)*Guild Information",soup).group(0).splitlines()[12]
+    #print(guild_name)
+    server = re.search("The guild was founded on (.*) on",soup).group(1)
+    text =  re.search(".*Joining DateStatus(.|\n)*Invited Characters",soup).group(0).splitlines()
+    if 'leadership of this guild' in text[-2]:
+        if 'offline' in text[-2]:
+            text[-2]='offline '
+        else:
+            text[-2]='online '
+        text.pop()
+    else:
+        if 'offline' in text[-1]:
+            text[-1]='offline '
+        else:
+            text[-1]='online '
+    text.pop(0)
+    def divide_chunks(l, n):
+        for i in range(0, len(l), n): 
+            yield l[i:i + n]
+    n = 5
+    x = list(divide_chunks(text, n))
+    if server not in whole_list:
+        whole_list[server] = []
+    if server in total:
+        total[server]= total[server] + int(len(x))
+    else:
+        total[server]= int(len(x))
+    for each in x:
+        #print(each)
+        if "online" in each[4]:
+            whole_list[server].append([each[0],each[1],each[2],guild_name])
+        #print(each)
 def sort_and_html(lista):
     global html
     global total
@@ -132,7 +131,7 @@ def sort_and_html(lista):
     html+='<style>body {color: #eee;background-color:#2f3136;}a{color: #1CB0F4;} h1, h2, h3 { margin: 0; }</style>'
     html+='</head>'
     html+="<body>"
-    html+="<center><h1>Guildbank Guilds</h1></center>"
+    html+="<center><h1>Army Invaders</h1></center>"
     now = datetime.now()
     html+=" <center><h4>Last Updated: "+now.strftime("%m/%d/%Y %H:%M:%S")+"</h4></center>\n"
     html+='<table style="border:1px solid black; margin-left:auto;margin-right:auto;"><tr>'
@@ -163,10 +162,7 @@ def sort_and_html(lista):
                 if voc != member[1]:
                     html+='<tr><td colspan="2"><center><h3>'+member[1]+"</center></h3></td></tr>"
                     voc = member[1]
-                if member[0] not in commanders:
-                    player = member[0]
-                else:
-                    player='<mark>'+member[0]+"</mark>"
+                player = member[0]
                 if member[2] > 249:
                     color = "#FFFFFF"
                 else:
@@ -179,9 +175,6 @@ def sort_and_html(lista):
     html+="</body>"
     html+="</html>"
 
-
-
-asyncio.run(get_symbols())
 
 sort_and_html(whole_list)
 
